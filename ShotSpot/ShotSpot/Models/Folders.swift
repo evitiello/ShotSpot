891674b535a6e8ad9975a27c0de611f273b4bba6
Eric Vitiello <eric@perceive.net>
2022-06-02 17:12:21 -0700

    getting there. need to get access to the desktop folder.

--- ShotSpot/ShotSpot/Models/Folders.swift
+++ ShotSpot/ShotSpot/Models/Folders.swift
@@ -14,8 +14,12 @@ final class Folders: Combine.ObservableObject {
 	@Published var all: [Folder] = []
 	
 	init() {
-//		var urls: [URL]? = FileManager.default.urls(for: .desktopDirectory)
-//		var someFolders = urls.map { Folder(url: $0[0]) }
+		var urls: [URL]? = FileManager.default.urls(for: .desktopDirectory)
+		
+		guard let cleanURLs = urls else { return }
+		
+		//self.all = urls.map { Folder(url: $0[0]) }
+
 		self.all = [
 			Folder(url: URL(fileURLWithPath: "/Library")),
 			Folder(url: URL(fileURLWithPath: "/Library")),
@@ -29,5 +33,5 @@ final class Folders: Combine.ObservableObject {
 struct Folder {
 	var url: URL = URL(fileURLWithPath: "/Library")
 	var name = "BLAH"
-	var id = "UNIQUEID"
+	var id = UUID()
 }
